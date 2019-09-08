3e18585b8d9e1e2e417d50626731f568cd0ffb28
Barış Çimen <barcimen@gmail.com>
2019-09-08 23:41:23 +0300

    Docker fix

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/#!/usr/bin/env bash
 chown -R www:www /var/www # Dockerfile sometimes fails to change ownerships, so I added this here to ensure ...
-sudo -u www composer install
 sudo -u www cp .env.docker .env
+sudo -u www composer install
 sudo -u www php artisan key:generate
 sudo -u www php artisan migrate:refresh --seed
